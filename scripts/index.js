55c2e5cacb44ec525d4146cd933547d9cb68f007
MohammedHany123 <midohany1807@gmail.com>
2025-04-20 20:26:20 +0200

    homepage javascript

---
+++ scripts/index.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userData = JSON.parse(localStorage.getItem("currentUser")); // whole user object
+    const allCards = document.querySelectorAll(".user-card");
+    const wayHeader = document.querySelector(".way-header");
+    const rightSec = document.getElementById("rightSec");
+    const quickLinksSection = document.querySelector(".quick-links");
+  
+    if (userData && userData.role === "teacher") {
+        const teacherName = userData.name;
+        const rows = document.querySelectorAll(".task-table tbody tr");
+
+        rows.forEach(row=>{
+            const assignedBy = row.cells[3]?.textContent.trim();
+            if (assignedBy != teacherName) {
+                row.style.display = "none";
+            }
+        });
+    }
+
+    if (userData) {
+        const { role, name } = userData;
+    
+        allCards.forEach(card => {
+            const roleText = card.querySelector("h1").innerText.toLowerCase();
+            if (roleText !== role) card.style.display = "none";
+        });
+    
+        wayHeader.innerText = `Welcome, ${name}!`;
+    
+        document.querySelectorAll(".links").forEach(link => {
+            const text = link.innerText.toLowerCase();
+            if (text.includes("login") || text.includes("sign up")) {
+            link.style.display = "none";
+            }
+        });
+    
+        if (quickLinksSection) {
+            quickLinksSection.style.display = "none";
+        }
+    
+        const dashboardLink = document.createElement("a");
+        dashboardLink.className = "links";
+        dashboardLink.href = `pages/dashboard/${capitalize(role)}Dashboard.html`;
+        dashboardLink.innerText = "Dashboard";
+        rightSec.appendChild(dashboardLink);
+    
+        const logout = document.createElement("a");
+        logout.className = "links";
+        logout.href = "#";
+        logout.innerText = "Logout";
+        logout.addEventListener("click", () => {
+            localStorage.clear();
+            location.reload();
+        });
+        rightSec.appendChild(logout);
+    }
+    
+    function capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+});
+  
